08efb452cf42d43e81f89e0399a5d1378c1e49b3
mikemunevar <mikemunevar@gmail.com>
2017-02-10 02:45:01 +0000

    Updated the activeadmin dashboard view.

--- app/admin/block.rb
+++ app/admin/block.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register Block do
 
+
+#MDM
+    permit_params :title, :show_title, :body, :position, :display, :class_suffix, :order, :is_published
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -13,5 +16,32 @@ ActiveAdmin.register Block do
 #   permitted
 # end
 
+#MDM
+    index do
+        column :id
+        column :title,      :sortable => :title
+        column :position,    :sortable => :position
+        column :display,    :sortable => :display
+        column :created_at, :sortable => :created_at
+        column :order
+        actions
+    end
+#MDM
+    form do |f|
+       f.inputs "Details" do
+            f.input :title,         :label => "Title"
+            f.input :show_title,    :label => "Show Title"
+            f.input :body,          :label => "Body"
+            
+            f.input :position,      :label => "Position", :as => :select, :collection => [["Jumbotron", "jumbotron"], ["Block", "block"]] 
+            f.input :display,       :label => "Display", :as => :select, :collection => [["All pages", "all"], ["Homepage Only", "home"], ["All But Homepage", "nohome"]] 
+            
+            f.input :order,         :label => "Order"
+            f.input :class_suffix,  :label => "Class Suffix"
+            f.input :is_published,  :label => "Published"
+
+            f.actions
+       end
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209221124) do
+ActiveRecord::Schema.define(version: 20170210012212) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170209221124) do
     t.boolean  "is_published"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "order"
   end
 
   create_table "pages", force: :cascade do |t|
