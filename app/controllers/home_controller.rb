0e40ea31856301e92f4464c324b9f548019dbf7d
mikemunevar <mikemunevar@gmail.com>
2017-03-15 00:48:37 +0000

    Fixed the view of the About Us and Web Design page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def show
-    @page = page.find(params[:id])
-    if @page.ispublihsed == false
+    @page = Page.find(params[:id])
+    if @page.is_published == false
       redirect_to root_path, alert:"This page does not exist."
+    end
+  
+    @sections = Section.all
   end
 end
