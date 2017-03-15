4841b4502880fbf2c0660d8f6b01699ebe32b1d2
mikemunevar <mikemunevar@gmail.com>
2017-03-15 00:10:24 +0000

    Added some code to show the nav on the menu, although it doesn't work.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  #MDM
+  before_filter :getPageNav
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #MDM xxxxxxxxxxxxxxxxxxxxx
+  def getPageNav
+  # @pageNav = Page.where("menu_display = true && is_published = true").order(order: :asc)
+  # @pageNav = Page.where("menu_display = true").order(order: :asc)
+    @pageNav = Page.where("menu_display = ? and is_published = ?").order(order: :asc)
+
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def show
+    @page = page.find(params[:id])
+    if @page.ispublihsed == false
+      redirect_to root_path, alert:"This page does not exist."
   end
 end
